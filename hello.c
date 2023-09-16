6f14a201e452150dac28d4b52cb3bd03f9544621
CarolGitonga <carolgitongaofficial@gmail.com>
2023-09-16 12:31:01 +0300

    first commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //This is the header file
+
+int main() {//This is the main function
+  printf("Hello World!");//This is for printing the statement
+  printf("This is my first time learning C \n");
+  printf("This is for introducing a new line\n");
+  printf("You can also introduce\n new multiple lines\n \n as these characters create a blank line\n");
+  printf("This is introducing \t an horizontal tab\n");
+  printf("This is for inserting a\\ backlash character\n");
+  printf("This inserts a \" double quote character");
+  return 0;//returns an integer value of 0 from the main function.
+}
